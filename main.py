0354feb9ebd2934867954f699ff27438fe3df7ec
MaskDeSmith <jasondylanelmer@gmail.com>
2025-01-29 08:06:18 -0800

    created shot/shot applicatoion, kill functions, organized/clean code

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
 import pygame
 from constants import *
-from player import *
-from asteroid import *
-from asteroidfield import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 import sys
 
 def main():
     pygame.init()
+    print("Starting asteroids!")
+    
     frame_rate = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     
     Asteroid.containers = (asteroids, updatable, drawable)
     Player.containers = (updatable, drawable)
     AsteroidField.containers = (updatable,)
+    Shot.containers = (shots, updatable, drawable)
     
-    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
 
     while True:
@@ -30,15 +32,26 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
-        dt = frame_rate.tick(60) / 1000
+
         for object in updatable:
             object.update(dt)
-        for object in asteroids:
-            if object.collision(player):
+
+        for asteroid in asteroids:
+            if asteroid.collision(player):
                 print("Game over!")
                 sys.exit()
+
+            for shot in shots:
+                if asteroid.collision(shot):
+                    shot.kill()
+                    asteroid.kill()
+
         for object in drawable:
             object.draw(screen)
+
         pygame.display.flip()
+    
+        dt = frame_rate.tick(60) / 1000
+
 if __name__ == "__main__":
     main()
